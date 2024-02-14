d790d7b9d40b70f118ececb5c15e85c800937e71
Kole Kikuta <ktkikuta@ucsb.edu>
2024-02-13 18:10:22 -0800

    fixtures file created

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,37 @@
+const ucsbOranizationFixtures = {
+    oneOrganization:
+    [
+        {
+            "id": 1,
+            "orgCode": "NSU",
+            "orgTranslationShort": "Nikkei Student Union",
+            "orgTranslation": "UCSB Nikkei Student Union"
+        }
+    ],
+
+    threeOrganizations:
+    [
+        {
+            "id": 1,
+            "orgCode": "NSU",
+            "orgTranslationShort": "Nikkei Student Union",
+            "orgTranslation": "UCSB Nikkei Student Union"
+        },
+
+        {
+            "id": 2,
+            "orgCode": "TT",
+            "orgTranslationShort": "Theta Tau",
+            "orgTranslation": "UCSB Theta Tau"
+        },
+
+        {
+            "id": 3,
+            "orgCode": "FIERCE",
+            "orgTranslationShort": "UCSB FIERCE",
+            "orgTranslation": "UCSB FIERCE Outreach"
+        },
+    ]
+};
+
+export { ucsbOranizationFixtures };
