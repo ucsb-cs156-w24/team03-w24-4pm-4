718b9aa45ef3f7fc6e465c9b839904d93b4b7cf6
Kole Kikuta <ktkikuta@ucsb.edu>
2024-02-15 15:40:17 -0800

    form tests written

--- frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.tests.js
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.tests.js
@@ -1,10 +1,7 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
-
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
 import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
-import { ucsbOrganizationFixturess } from "fixtures/ucsbOrganizationFixtures";
-
-import { QueryClient, QueryClientProvider } from "react-query";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const mockedNavigate = jest.fn();
 
@@ -13,85 +10,121 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockedNavigate
 }));
 
+
 describe("UCSBOrganizationForm tests", () => {
-    const queryClient = new QueryClient();
 
-    const expectedHeaders = ["orgCode", "orgTranslationShort", "orgTranslation", "inactive"];
-    const testId = "UCSBOrganizationForm";
+    test("renders correctly", async () => {
 
-    test("renders correctly with no initialContents", async () => {
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <UCSBOrganizationForm />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <UCSBOrganizationForm />
+            </Router>
         );
+        await screen.findByText(/orgCode/);
+        await screen.findByText(/Create/);
+    });
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+    test("renders correctly when passing in a UCSBOrganization", async () => {
+
+        render(
+            <Router  >
+                <UCSBOrganizationForm initialContents={ucsbOrganizationFixtures.oneOrganization} />
+            </Router>
+        );
+        await screen.findByTestId(/UCSBOrganizationForm-id/);
+        expect(screen.getByText(/orgCode/)).toBeInTheDocument();
+        expect(screen.getByTestId(/UCSBOrganizationForm-id/)).toHaveValue("1");     //prob dont need
+    });
+
+
+    test("Correct Error messsages on bad input", async () => {
+
+        render(
+            <Router  >
+                <UCSBOrganizationForm />
+            </Router>
+        );
+        await screen.findByTestId("UCSBOrganizationForm-orgCode");
+        const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+        const orgTranslationShort = screen.getByTestId("UCSBOrganizationForm-orgTranslationShort");
+        const orgTranslation = screen.getByTestId("UCSBOrganizationForm-orgTranslation");
+        const inactive = screen.getByTestId("UCSBOrganizationForm-inactive");
+        const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
+
+        fireEvent.change(orgCodeField, { target: { value: 'bad-input' } });
+        fireEvent.change(orgTranslationShort, { target: { value: 'bad-input' } });
+        fireEvent.change(orgTranslation, { target: {value: 'bad-input' } });
+        fireEvent.change(inactive, { target: { value: 'bad-input' } });
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/The input should be just true or false/);
+    });
+
+    test("Correct Error messsages on missing input", async () => {
+
+        render(
+            <Router  >
+                <UCSBOrganizationForm />
+            </Router>
+        );
+        await screen.findByTestId("UCSBOrganizationForm-submit");
+        const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/orgCode is required./);
+        expect(screen.getByText(/orgTranslationShort is required./)).toBeInTheDocument();
+        expect(screen.getByText(/orgTranslation is required./)).toBeInTheDocument();
+        expect(screen.getByText(/inactive is required./)).toBeInTheDocument();
 
     });
 
-    test("renders correctly when passing in a UCSBOrganization", async () => {
+    test("No Error messsages on good input", async () => {
+
+        const mockSubmitAction = jest.fn();
+
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <UCSBOrganizationForm initialContents={ucsbOrganizationFixturess.oneOrganization} />
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <UCSBOrganizationForm submitAction={mockSubmitAction} />
+            </Router>
         );
+        await screen.findByTestId("UCSBOrganizationForm-orgCode");
 
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
+        await screen.findByTestId("UCSBOrganizationForm-orgCode");
+        const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
+        const orgTranslationShort = screen.getByTestId("UCSBOrganizationForm-orgTranslationShort");
+        const orgTranslation = screen.getByTestId("UCSBOrganizationForm-orgTranslation");
+        const inactive = screen.getByTestId("UCSBOrganizationForm-inactive");
+        const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
 
-        expectedHeaders.forEach((headerText) => {
-            const header = screen.getByText(headerText);
-            expect(header).toBeInTheDocument();
-        });
+        fireEvent.change(orgCodeField, { target: { value: 'NSU' } });
+        fireEvent.change(orgTranslationShort, { target: { value: 'Nikkei Student Union' } });
+        fireEvent.change(orgTranslation, { target: {value: 'UCSB Nikkei Student Union' } });
+        fireEvent.change(inactive, { target: { value: 'false' } });
+        fireEvent.click(submitButton);
 
-        await screen.findByTestId(/UCSBOrganizationForm-id/);
-        expect(screen.getByText(/orgCode/)).toBeInTheDocument();
-        expect(screen.getByTestId(/UCSBOrganizationForm-id/)).toHaveValue("1");
+        await waitFor(() => expect(mockSubmitAction).toHaveBeenCalled());
+
+        expect(screen.queryByText(/The input should be just true or false/)).not.toBeInTheDocument();
+
     });
 
 
     test("that navigate(-1) is called when Cancel is clicked", async () => {
+
         render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <OrganizationForm/>
-                </Router>
-            </QueryClientProvider>
+            <Router  >
+                <UCSBOrganizationForm />
+            </Router>
         );
-        expect(await screen.findByTestId(`UCSBOrganization-cancel`)).toBeInTheDocument();
-        const cancelButton = screen.getByTestId(`UCSBOrganization-cancel`);
+        await screen.findByTestId("UCSBOrganizationForm-cancel");
+        const cancelButton = screen.getByTestId("UCSBOrganizationForm-cancel");
 
         fireEvent.click(cancelButton);
 
         await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
-    });
-
-    test("Correct Error messsages on missing input", async () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <UCSBOrganizationForm />
-                </Router>
-            </QueryClientProvider>
-        );
-
-        expect(await screen.findByText(/Create/)).toBeInTheDocument();
-        const submitButton = screen.getByText(/Create/);
-        fireEvent.click(submitButton);
-
-        await screen.findByText(/orgCode is required/);
-        expect(screen.getByText(/orgTranslationShort is required/)).toBeInTheDocument();
-        expect(screen.getByText(/orgTranslationt is required/)).toBeInTheDocument();
-        expect(screen.getByText(/inactive is required/)).toBeInTheDocument();
 
     });
 
