dde72d8f2ed4f51aab789ed6444f0a8b3426c8a5
Justin Zhang <justinzhang@ucsb.edu>
2024-02-15 16:25:16 -0800

    JustinZ - Create Fixtures for UCSBDiningCommonsMenuItems

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,38 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+    oneMenuItem:
+    [
+      {
+       "id": 1,
+       "diningCommonsCode": "ortega",
+       "name": "Baked Pesto Pasta with Chicken",
+       "station": "Entree Specials"
+      }
+    ],
+
+    threeMenuItem:
+    [
+        {
+            "id": 2,
+            "diningCommonsCode": "ortega",
+            "name": "Tofu Banh Mi Sandwich (v)",
+            "station": "Entree Specials"
+        },
+
+        {
+            "id": 3,
+            "diningCommonsCode": "ortega",
+            "name": "Chicken Caesar Salad",
+            "station": "Entree"  
+        },
+
+        {
+            "id": 4,
+            "diningCommonsCode": "portola",
+            "name": "Cream of Broccoli Soup (v)",
+            "station": "Greens and Grains"
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
