03735dc3b9f771ee91ff7ce7c30ecd59d2c1639f
Hongye Liang <hongye_liang@ucsb.edu>
2024-02-16 21:50:04 -0800

    fix small issue with component and test

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -22,157 +22,6 @@ function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Crea
     // Stryker disable next-line Regex
     const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
-    // Stryker disable next-line all
-    //const yyyyq_regex = /((19)|(20))\d{2}[1-4]/i; // Accepts from 1900-2099 followed by 1-4.  Close enough.
-/*
-    return (
-
-        <Form onSubmit={handleSubmit(submitAction)}>
-
-
-            <Row>
-
-                {initialContents && (
-                    <Col>
-                        <Form.Group className="mb-3" >
-                            <Form.Label htmlFor="id">Id</Form.Label>
-                            <Form.Control
-                                data-testid="MenuItemReviewForm-id"
-                                id="id"
-                                type="text"
-                                {...register("id")}
-                                value={initialContents.id}
-                                disabled
-                            />
-                        </Form.Group>
-                    </Col>
-                )}
-            </Row>
-
-            <Row>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="itemId">Item ID</Form.Label>
-                        <Form.Control
-                            data-testid="MenuItemReviewForm-itemId"
-                            id="itemId"
-                            type="text"
-                            isInvalid={Boolean(errors.itemId)}
-                            {...register("itemId", { required: "Item Id is required" })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.itemId && 'itemId is required. '}
-                            {errors.itemId?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-            </Row>
-            
-
-            <Row>
-                <col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="reviewerEmail">Reviewer Email</Form.Label>
-                        <Form.Control
-                            data-testid={"MenuItemReviewForm-reviewerEmail"}
-                            id="reviewerEmail"
-                            type="text"
-                            isInvalid={Boolean(errors.name)}
-                            {...register("reviewerEmail", {
-                                required: "reviewerEmail is required."
-                            })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.reviewerEmail?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </col>
-            </Row>
-
-
-            <Row>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="stars">Stars</Form.Label>
-                        <Form.Control
-                            data-testid="MenuItemReviewForm-stars"
-                            id="stars"
-                            type="text"
-                            isInvalid={Boolean(errors.itemId)}
-                            {...register("stars", { required: "Stars is required" })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.stars && 'stars is required.'}
-                            {errors.stars?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-            </Row>
-            
-            <Row>
-                <Col>
-                    <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="dateReviewed">Date Reviewed (iso format)</Form.Label>
-                        <Form.Control
-                            data-testid="MenuItemReviewForm-dateReviewed"
-                            id="dateReviewed"
-                            type="datetime-local"
-                            isInvalid={Boolean(errors.dateReviewed)}
-                            {...register("dateReviewed", { required: true, pattern: isodate_regex })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.dateReviewed && 'dateReviewed is required in iso format.'}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-            </Row>
-
-            <Row>
-
-                <Col>
-
-                <Form.Group className="mb-3" >
-                        <Form.Label htmlFor="comments">Comments</Form.Label>
-                        <Form.Control
-                            data-testid="MenuItemReviewForm-comments"
-                            id="comments"
-                            type="text"
-                            isInvalid={Boolean(errors.name)}
-                            {...register("comments", {
-                                required: "comments are required."
-                            })}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {errors.comments?.message}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-            </Row>
-
-            <Row>
-                <Col>
-                    <Button
-                        type="submit"
-                        data-testid="MenuItemReviewForm-submit"
-                    >
-                        {buttonLabel}
-                    </Button>
-                    <Button
-                        variant="Secondary"
-                        onClick={() => navigate(-1)}
-                        data-testid="MenuItemReviewForm-cancel"
-                    >
-                        Cancel
-                    </Button>
-                </Col>
-            </Row>
-        </Form>
-
-    )
-}
-
-export default MenuItemReviewForm;
-*/
 
 return (
 
@@ -208,7 +57,6 @@ return (
                             {...register("itemId", { required: "Item Id is required" })}
                         />
                         <Form.Control.Feedback type="invalid">
-                            {errors.itemId && 'itemId is required.'}
                             {errors.itemId?.message}
                         </Form.Control.Feedback>
                     </Form.Group>
@@ -221,7 +69,7 @@ return (
                         data-testid={"MenuItemReviewForm-reviewerEmail"}
                         id="reviewerEmail"
                         type="text"
-                        isInvalid={Boolean(errors.name)}
+                        isInvalid={Boolean(errors.reviewerEmail)}
                         {...register("reviewerEmail", {
                             required: "reviewerEmail is required."
                         })}
@@ -244,7 +92,6 @@ return (
                         {...register("stars", { required: "Stars is required" })}
                     />
                     <Form.Control.Feedback type="invalid">
-                        {errors.stars && 'stars is required.'}
                         {errors.stars?.message}
                     </Form.Control.Feedback>
                 </Form.Group>
@@ -271,15 +118,13 @@ return (
 
             <Col>
 
-
-
                 <Form.Group className="mb-3" >
                     <Form.Label htmlFor="comments">Comments</Form.Label>
                     <Form.Control
                         data-testid="MenuItemReviewForm-comments"
                         id="comments"
                         type="text"
-                        isInvalid={Boolean(errors.name)}
+                        isInvalid={Boolean(errors.comments)}
                         {...register("comments", {
                             required: "comments are required."
                         })}
