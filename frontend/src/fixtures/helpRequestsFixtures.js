83caaf9f17c5e70c6f9f6b5ce2beee156209a3b7
Rohan Marangoly <rmarangoly@ucsb.edu>
2024-02-17 17:10:57 -0800

    added util tests

--- frontend/src/fixtures/helpRequestsFixtures.js
+++ frontend/src/fixtures/helpRequestsFixtures.js
@@ -2,9 +2,9 @@ const helpRequestsFixtures = {
     oneRequest: {
         "id": 1,
         "requesterEmail": "sampleemail1@gmail.com",
-        "teamId": "string",
+        "teamId": "w24-4pm-4",
         "tableOrBreakoutRoom": "string",
-        "requestTime": "2024-02-14T02:13:33.833Z",
+        "requestTime": "2024-02-14T02:13:33",
         "explanation": "string",
         "solved": true
     },
@@ -12,27 +12,27 @@ const helpRequestsFixtures = {
         {
             "id": 1,
             "requesterEmail": "sampleemail2@gmail.com",
-            "teamId": "w24-4pm-1",
+            "teamId": "w24-4pm-4",
             "tableOrBreakoutRoom": "string",
-            "requestTime": "2024-02-14T02:13:33.833Z",
+            "requestTime": "2024-02-14T02:13:33",
             "explanation": "string",
             "solved": true
         },
         {
             "id": 2,
             "requesterEmail": "sampleemail3@gmail.com",
-            "teamId": "w24-4pm-2",
+            "teamId": "w24-4pm-4",
             "tableOrBreakoutRoom": "sstring",
-            "requestTime": "2024-03-14T02:13:23.833Z",
+            "requestTime": "2024-03-14T02:13:23",
             "explanation": "strings",
             "solved": false
         },
         {
             "id": 3,
             "requesterEmail": "sampleemail4@gmail.com",
-            "teamId": "w24-4pm-3",
+            "teamId": "w24-4pm-4",
             "tableOrBreakoutRoom": "strasing",
-            "requestTime": "2024-02-14T02:13:13.833Z",
+            "requestTime": "2024-02-14T02:13:13",
             "explanation": "strisdang",
             "solved": true
         }
