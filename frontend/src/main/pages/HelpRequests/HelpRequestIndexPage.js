059520becab0ca49a1bf6db7303f60905fb65794
Rohan Marangoly <rmarangoly@ucsb.edu>
2024-02-18 22:37:29 -0800

    added index

--- frontend/src/main/pages/HelpRequests/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequests/HelpRequestIndexPage.js
@@ -1,14 +1,43 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import HelpRequestsTable from 'main/components/HelpRequests/HelpRequestsTable';
+import { Button } from 'react-bootstrap';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser';
 
 export default function HelpRequestIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        return (
+            <Button
+                variant="primary"
+                href="/helprequests/create"
+                style={{ float: "right" }}
+            >
+                Create Help Request 
+            </Button>
+        )
+    } 
+  }
+  
+  const { data: requests, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/helprequests/all"],
+      { method: "GET", url: "/api/helprequests/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/placeholder/create">Create</a></p>
-        <p><a href="/placeholder/edit/1">Edit</a></p>
+        {createButton()}
+        <h1>Help Requests</h1>
+        <HelpRequestsTable helpRequests={requests} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
