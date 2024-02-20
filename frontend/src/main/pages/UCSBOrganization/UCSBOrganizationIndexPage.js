e869fda22925cc9b36a94e2c3963ca8f3e3c5c11
Kole Kikuta <ktkikuta@ucsb.edu>
2024-02-20 00:03:07 -0800

    testing finished

--- frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
+++ frontend/src/main/pages/UCSBOrganization/UCSBOrganizationIndexPage.js
@@ -1,14 +1,44 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBOrganizationTable from 'main/components/UCSBOrganization/UCSBOrganizationTable';
+import { Button } from 'react-bootstrap';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser';
 
 export default function UCSBOrganizationIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+        return (
+            <Button
+                variant="primary"
+                href="/ucsborganization/create"
+                style={{ float: "right" }}
+            >
+                Create UCSBOrganization
+            </Button>
+        )
+    }
+  }
+
+  const { data: organizations, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/ucsborganization/all"],
+      { method: "GET", url: "/api/ucsborganization/all" },
+      // Stryker disable next-line all : don't test default value of empty list
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/ucsborganization/create">Create</a></p>
-        <p><a href="/ucsbOrganization/edit/1">Edit</a></p>
+        {createButton()}
+        <h1>UCSBOrganization</h1>
+        <UCSBOrganizationTable orgs={organizations} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
