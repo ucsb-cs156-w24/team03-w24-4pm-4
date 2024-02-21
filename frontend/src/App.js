d4a093906882ed9a8690b8d7a06e6a4579b0d834
hongye-liang <97082279+hongye-liang@users.noreply.github.com>
2024-02-20 20:57:52 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -88,21 +88,7 @@ function App() {
             </>
           )
         }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/articles" element={<ArticlesIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && (
-            <>
-              <Route exact path="/articles/edit/:id" element={<ArticlesEditPage />} />
-              <Route exact path="/articles/create" element={<ArticlesCreatePage />} />
-            </>
-          )
-        }
+    
         {
           hasRole(currentUser, "ROLE_USER") && (
             <>
