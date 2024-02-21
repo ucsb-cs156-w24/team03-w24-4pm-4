cd934e4dd6e524fa1c3f7b1f1ece08b058b457d1
Kole Kikuta <ktkikuta@ucsb.edu>
2024-02-20 18:01:58 -0800

    fixed lint

--- frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
+++ frontend/src/main/components/UCSBOrganization/UCSBOrganizationTable.js
@@ -42,7 +42,6 @@ export default function UCSBOrganizationTable({ orgs, currentUser }) {
         },
         {
             Header: 'inactive',
-            accessor: 'inactive',
             accessor: d => d.inactive ? "true" : "false",
         }
     ];
